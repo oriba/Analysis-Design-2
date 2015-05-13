77cd6a1d96ee370f70233014d7a2fbb4065f81fa
lihiv <lihiv92@gmail.com>
2015-05-13 15:10:19 +0300

    created order screen for coupon

--- newCoupons - curr/SQLQuery1.sql
+++ newCoupons - curr/SQLQuery1.sql
@@ -1,4 +1,4 @@
-﻿insert into CouponMaker
+insert into CouponMaker
 
 select ID=1,name='mcCoupon',[description]='best price', originalPrice=50,couponPrice=25,rating=2,numOfRaters=1,startDate='2015-04-04',endDate='2015-05-05',quantity=0,maxQuantity=100,statusID='Active',BusinessID=1
 
@@ -6,3 +6,12 @@ use master
 alter database Coupons1
 	set SINGLE_USER with rollback immediate
 alter database Coupons1 SET MULTI_USER
+
+
+        <div class="form-group">
+            @Html.LabelFor(model => model.Status, htmlAttributes: new { @class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.EnumDropDownListFor(model => model.Status, htmlAttributes: new { @class = "form-control" })
+                @Html.ValidationMessageFor(model => model.Status, "", new { @class = "text-danger" })
+            </div>
+        </div>
