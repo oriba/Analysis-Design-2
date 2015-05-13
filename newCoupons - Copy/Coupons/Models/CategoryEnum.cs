81a2401e6a54d401e20bf06d4c604ba993d69293
LItal  Morali <litalmor5@gmail.co.il>
2015-05-13 11:54:23 +0300

    copy of Coupons

---
+++ newCoupons - Copy/Coupons/Models/CategoryEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coupons.Models
+{
+    public enum CategoryEnum
+    {
+        Food, 
+        Entertainment, 
+        HealthAndBeauty, 
+        Apparel, 
+        Electronics //TODO: edit statuses
+    }
+}
---
+++ newCoupons - Copy/Coupons/Models/CouponMaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coupons.Models
+{
+
+    public class CouponMaker
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public int originalPrice { get; set; }
+        public int couponPrice { get; set; }
+        public int rating { get; set; }
+        public int numOfRaters { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public int quantity { get; set; }
+        public int maxQuantity { get; set; }
+        public string StatusID { get; set; }
+        public int BusinessID { get; set; }
+        
+        public virtual Business Business { get; set; }
+        public virtual Status Status { get; set; }
+    }
+}
